ce62cd2caa0d9ad1674b4aef69f7f57154e961fe
Victor Vilar <victor.svilar@gmail.com>
2022-02-01 14:06:07 -0300

    Documentando classe supervisor.

--- src/main/java/br/com/landtec/entities/Supervisor.java
+++ src/main/java/br/com/landtec/entities/Supervisor.java
@@ -6,15 +6,70 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * 
+ * @author Victor Vilar
+ * @since 2022-02-01
+ * class that represents a supervisor of a client.
+ */
+
 @Entity
 @Table(name="supervisors")
 public class Supervisor {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
 	private String name;
+	private String role;
 	private String phoneNumber;
 	private String email;
+	
+	
+	//constructors
+	public Supervisor() {
+		
+	}
+	//-----------
+	
+	//getters e setters - name
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	//-----------
+	
+	//getters e setters - role
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	//-------------
+	
+	
+	//getters e setters = phoneNumber
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	//--------------
+	
+	
+	//getters e setters - email
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	//--------------
+	
 	
 	
 }
