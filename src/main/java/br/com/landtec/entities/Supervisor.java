980b52f792564a5767982407dc494702e257cac3
Victor Vilar <victor.svilar@gmail.com>
2022-02-01 13:52:10 -0300

    implementando getters e setters e atributos da classe supervisor.

--- src/main/java/br/com/landtec/entities/Supervisor.java
+++ src/main/java/br/com/landtec/entities/Supervisor.java
@@ -1,5 +1,20 @@
 package br.com.landtec.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="supervisors")
 public class Supervisor {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String name;
+	private String phoneNumber;
+	private String email;
+	
+	
 }
