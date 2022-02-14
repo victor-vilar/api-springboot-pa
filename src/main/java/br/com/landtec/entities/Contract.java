af7421e0df5ace30208f5d1a02027ce7d08d37c2
Victor Vilar <victor.svilar@gmail.com>
2022-02-14 15:13:11 -0300

    implementado classe Contract.

--- src/main/java/br/com/landtec/entities/Contract.java
+++ src/main/java/br/com/landtec/entities/Contract.java
@@ -1,5 +1,17 @@
 package br.com.landtec.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="contracts")
 public class Contract {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String number;
+    private String validity;
 
 }
