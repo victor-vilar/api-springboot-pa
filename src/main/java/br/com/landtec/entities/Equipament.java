c8f84ced78ccd8a4dbadd740f64519cf5fa55a8b
Victor Vilar <victor.svilar@gmail.com>
2022-02-22 20:04:23 -0300

    criada classe RasidueType e implemenado suas propriedades e metodos.

--- src/main/java/br/com/landtec/entities/Equipament.java
+++ src/main/java/br/com/landtec/entities/Equipament.java
@@ -1,12 +1,53 @@
 package br.com.landtec.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Entity
 @Table(name="equipament")
 public class Equipament {
 	
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
 	private String equipamentName;
+	@Column(nullable = false)
 	private double sizeInMeterCubic;
-
+	
+	
+	//constructors
+	public Equipament() {
+		
+	}
+	//-----------
+	
+	
+	//getters e setters - name
+	public String getEquipamentName() {
+		return equipamentName;
+	}
+	public void setEquipamentName(String equipamentName) {
+		this.equipamentName = equipamentName;
+	}
+	//-----------
+	
+	
+	//getters e setters - size
+	public double getSizeInMeterCubic() {
+		return sizeInMeterCubic;
+	}
+	public void setSizeInMeterCubic(double sizeInMeterCubic) {
+		this.sizeInMeterCubic = sizeInMeterCubic;
+	}
+	//-----------
+	
+	
+	
+	
 }
