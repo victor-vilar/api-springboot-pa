1798f1b00a43953a6d35afa46e75239d374a1a9d
Victor Vilar <victor.svilar@gmail.com>
2022-02-26 19:14:52 -0300

    adiciionando nullable =false no nome e cnpj do cliente.

--- src/main/java/br/com/landtec/entities/Client.java
+++ src/main/java/br/com/landtec/entities/Client.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -29,7 +29,9 @@ public class Client {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable=false)
 	private String nameCompanyName;
+	@Column(nullable = false)
 	private String cpfCnpj;
 	
 	@OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
