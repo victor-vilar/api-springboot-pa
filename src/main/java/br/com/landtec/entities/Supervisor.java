120706ea09a78124b7c4abd54af56965fd6710f5
Victor Vilar <victor.svilar@gmail.com>
2022-03-03 09:00:05 -0300

    adicionando fetch.lazy nas cardinalidades manyToOne.

--- src/main/java/br/com/landtec/entities/Supervisor.java
+++ src/main/java/br/com/landtec/entities/Supervisor.java
@@ -2,6 +2,7 @@ package br.com.landtec.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -30,7 +31,7 @@ public class Supervisor {
 	@Column(nullable = false)
 	private String email;
 	
-	@ManyToOne
+	@ManyToOne(fetch=FetchType.LAZY)
 	private Client client;
 	
 	//constructors
