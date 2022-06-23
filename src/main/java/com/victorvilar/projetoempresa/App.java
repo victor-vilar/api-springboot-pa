b1f08968f89d2417faa00e4fb6d6906f801a3388
Victor Vilar <victor.svilar@gmail.com>
2022-06-23 10:30:47 -0300

    adicionada dependencias do Spring Boot e criada a classe principal App

---
+++ src/main/java/com/victorvilar/projetoempresa/App.java
@@ -0,0 +1,14 @@
+package com.victorvilar.projetoempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.limplace.sistema.SistemaApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
