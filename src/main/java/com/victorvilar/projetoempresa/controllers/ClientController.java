c6c0dcd3ef5c772236fb7fd73326637ccba0b4db
Victor Vilar <victor.svilar@gmail.com>
2022-06-27 14:45:34 -0300

    implementando metodos Get e Post no ClientController

--- src/main/java/com/victorvilar/projetoempresa/controllers/ClientController.java
+++ src/main/java/com/victorvilar/projetoempresa/controllers/ClientController.java
@@ -1,6 +1,39 @@
 package com.victorvilar.projetoempresa.controllers;
 
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorvilar.projetoempresa.entities.Client;
+
+@RestController
 public class ClientController {
 
+	private final ClientService as service;
+	
+	public ClientController(ClientService as service) {
+		this.service = service;
+	}
+	
+	/**
+	 * get all clients
+	 * @param as
+	 */
+	@GetMapping("/clients")
+	public List<Client> getClients(){
+		
+	}
+	
+	/**
+	 * Sing in a new Client
+	 * @param as
+	 */
+	@PostMapping("/clients/")
+	public void addNewClient(@RequestBody Client as client) {
+		this.service.addNewClient(client);
+	}
+	
 }
