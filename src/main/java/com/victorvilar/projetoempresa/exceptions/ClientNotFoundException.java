d03000e762c14ef3f328f9289a8ee67fc2c191a9
Victor Vilar <victor.svilar@gmail.com>
2022-10-17 17:55:36 -0300

    criada classe ClientNotFoundException para erros quando o cliente não for encontrado.

---
+++ src/main/java/com/victorvilar/projetoempresa/exceptions/ClientNotFoundException.java
@@ -0,0 +1,9 @@
+package com.victorvilar.projetoempresa.exceptions;
+
+public class ClientNotFoundException extends Exception{
+
+    public ClientNotFoundException(String msg){
+        super(msg);
+    }
+
+}
--- src/main/java/com/victorvilar/projetoempresa/services/ContractService.java
+++ src/main/java/com/victorvilar/projetoempresa/services/ContractService.java
@@ -1,7 +1,9 @@
 package com.victorvilar.projetoempresa.services;
 
+import com.victorvilar.projetoempresa.entities.Client;
 import com.victorvilar.projetoempresa.entities.Contract;
 import com.victorvilar.projetoempresa.entities.ItemContract;
+import com.victorvilar.projetoempresa.exceptions.ClientNotFoundException;
 import com.victorvilar.projetoempresa.repository.ClientRepository;
 import com.victorvilar.projetoempresa.repository.ContractRepository;
 
@@ -9,23 +11,32 @@ import java.util.List;
 
 public class ContractService {
 
-    private final ContractRepository repository;
+    private final ContractRepository contractRepository;
+    private final ClientRepository   clientRepository;
 
-    public ContractService (ContractRepository repository){
-        this.repository = repository;
+    public ContractService (ContractRepository repository, ClientRepository clienteRepository){
+        this.contractRepository = repository;
+        this.clientRepository = clienteRepository;
     }
 
 
     public List<Contract> getAllContracts() {
-        return repository.findAll();
+        return this.contractRepository.findAll();
     }
 
     public Contract getContractById(Long id) {
-        return repository.findById(id).orElse();
+        return this.contractRepository.findById(id).orElse();
     }
 
 
     public void addNewContract(Long clientId, List<ItemContract> itens) {
+        Client client = this.clientRepository.findById(clientId).orElseThrow(new ClientNotFoundException("This client don't exist"));
+        Contract contract = new Contract();
+        contract.setClient(client);
+        contract.setNumber();
+        contract.setValidity();
+
+
 
     }
 
