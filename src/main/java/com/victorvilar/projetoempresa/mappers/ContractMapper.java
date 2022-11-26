c6f6fc5097a1d3144062568c500d018072d987a9
Victor Vilar <victor.svilar@gmail.com>
2022-11-26 17:45:25 -0300

    criada classe ContractMapper.

---
+++ src/main/java/com/victorvilar/projetoempresa/mappers/ContractMapper.java
@@ -0,0 +1,38 @@
+package com.victorvilar.projetoempresa.mappers;
+
+import com.victorvilar.projetoempresa.controllers.dto.contract.ContractCreateDto;
+import com.victorvilar.projetoempresa.controllers.dto.contract.ContractResponseDto;
+import com.victorvilar.projetoempresa.domain.Contract;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ContractMapper {
+
+
+    private final ModelMapper mapper;
+
+    @Autowired
+    public ContractMapper(ModelMapper mapper){
+        this.mapper = mapper;
+    }
+
+
+    public Contract toContract(ContractCreateDto contractCreateDto){
+        return this.mapper.map(contractCreateDto, Contract.class);
+    }
+
+    public ContractResponseDto toContractResponseDto(Contract contract){
+        return this.mapper.map(contract,ContractResponseDto.class);
+    }
+
+    public List<ContractResponseDto> toContractResponsDtoList(List<Contract> contracts){
+        return contracts.stream().map(c ->this.toContractResponseDto(c)).collect(Collectors.toList());
+
+    }
+
+}
