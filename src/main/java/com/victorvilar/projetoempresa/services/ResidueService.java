f137e0d3fd36a09f2205d910d3d49dede7b49867
Victor Vilar <victor.svilar@gmail.com>
2022-12-01 19:56:43 -0300

    criada classe ResidueService.

---
+++ src/main/java/com/victorvilar/projetoempresa/services/ResidueService.java
@@ -0,0 +1,33 @@
+package com.victorvilar.projetoempresa.services;
+
+import com.victorvilar.projetoempresa.domain.ResidueType;
+import com.victorvilar.projetoempresa.exceptions.ResidueNotFoundException;
+import com.victorvilar.projetoempresa.repository.ResidueTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResidueService {
+
+    private final ResidueTypeRepository residueTypeRepository;
+
+    @Autowired
+    public ResidueService(ResidueTypeRepository residueTypeRepository){
+        this.residueTypeRepository = residueTypeRepository;
+    }
+
+    public ResidueType findById(Long id){
+        return this.residueTypeRepository.findById(id).orElseThrow(() -> new ResidueNotFoundException("This residue doesn't exist"));
+    }
+
+    public void deleteByid(Long id){
+        ResidueType residueType = this.findById(id);
+        this.residueTypeRepository.delete(residueType);
+    }
+
+    public void addNewResidueType(ResidueType residueType){
+        this.residueTypeRepository.save(residueType);
+    }
+
+
+}
