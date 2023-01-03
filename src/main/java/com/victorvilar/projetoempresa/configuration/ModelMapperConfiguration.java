fe399ef30b973e1be76bbcfcab099b3568e790cc
Victor Vilar <victor.svilar@gmail.com>
2023-01-03 15:29:31 -0300

    criada configuração para itemContract e ItemContractResponseDto na classe de configuração.

--- src/main/java/com/victorvilar/projetoempresa/configuration/ModelMapperConfiguration.java
+++ src/main/java/com/victorvilar/projetoempresa/configuration/ModelMapperConfiguration.java
@@ -1,8 +1,10 @@
 package com.victorvilar.projetoempresa.configuration;
 
 import com.victorvilar.projetoempresa.controllers.dto.contract.ContractResponseDto;
+import com.victorvilar.projetoempresa.controllers.dto.contract.ItemContractResponseDto;
 import com.victorvilar.projetoempresa.controllers.dto.supervisor.SupervisorResponseDto;
 import com.victorvilar.projetoempresa.domain.Contract;
+import com.victorvilar.projetoempresa.domain.ItemContract;
 import com.victorvilar.projetoempresa.domain.Supervisor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +26,12 @@ public class ModelMapperConfiguration {
         mapper.typeMap(Contract.class, ContractResponseDto.class)
                 .addMappings(maper ->{
                     maper.map(src -> src.getClient().getCpfCnpj(), ContractResponseDto::setClientId);
+                });
+
+        mapper.typeMap(ItemContract.class, ItemContractResponseDto.class)
+                .addMappings(maper ->{
+                    maper.map(src-> src.getResidue().getType(), ItemContractResponseDto::setResidue);
+                    maper.map(src-> src.getEquipament().getEquipamentName(),ItemContractResponseDto::setEquipament);
                 });
 
         return mapper;
