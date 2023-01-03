c99aaec62e20ef672256e20be0d7a97ee89bb711
Victor Vilar <victor.svilar@gmail.com>
2023-01-03 15:26:27 -0300

    criado metodo toItemContractResponseDto na classe ItemContractMapper.

--- src/main/java/com/victorvilar/projetoempresa/mappers/ItemContractMapper.java
+++ src/main/java/com/victorvilar/projetoempresa/mappers/ItemContractMapper.java
@@ -1,6 +1,7 @@
 package com.victorvilar.projetoempresa.mappers;
 
 import com.victorvilar.projetoempresa.controllers.dto.contract.ItemContractCreateDto;
+import com.victorvilar.projetoempresa.controllers.dto.contract.ItemContractResponseDto;
 import com.victorvilar.projetoempresa.domain.ItemContract;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class ItemContractMapper {
 
     public ItemContract toItemContract(ItemContractCreateDto itemDto){
         return this.mapper.map(itemDto,ItemContract.class);
+    }
+
+    public ItemContractResponseDto toItemContractResponseDto(ItemContract item){
+        return this.mapper.map(item,ItemContractResponseDto.class);
     }
 
 }
