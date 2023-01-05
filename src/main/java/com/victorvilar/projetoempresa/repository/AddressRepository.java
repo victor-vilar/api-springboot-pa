2c909a54df88c4e169d91dd2bf38365eedec3b30
Victor Vilar <victor.svilar@gmail.com>
2023-01-05 15:40:03 -0300

    adicionando query para pesquisa de endereços por um cliente.

--- src/main/java/com/victorvilar/projetoempresa/repository/AddressRepository.java
+++ src/main/java/com/victorvilar/projetoempresa/repository/AddressRepository.java
@@ -1,10 +1,14 @@
 package com.victorvilar.projetoempresa.repository;
 
+import com.victorvilar.projetoempresa.domain.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.victorvilar.projetoempresa.embendable.AddressId;
 import com.victorvilar.projetoempresa.domain.Address;
 
+import java.util.List;
+
 public interface AddressRepository extends JpaRepository<Address,AddressId> {
 
+    List<Contract> findByClientCpfCnpj(String clientId);
 }
