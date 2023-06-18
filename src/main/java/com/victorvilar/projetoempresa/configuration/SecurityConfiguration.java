65d9deb9514c3a5c9bbc9e3b3f6bbb9fb3e05d31
Victor Vilar <victor.svilar@gmail.com>
2023-06-18 14:22:34 -0300

    configurado spring security, adicionado classe SystemUser.

--- src/main/java/com/victorvilar/projetoempresa/configuration/SecurityConfiguration.java
+++ src/main/java/com/victorvilar/projetoempresa/configuration/SecurityConfiguration.java
@@ -5,8 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,26 +20,48 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(12);
+        return NoOpPasswordEncoder.getInstance();
     }
+
+
+    @Bean
+    public UserDetailsService userDetails(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                //cors configuration
                 .cors(cors ->cors
                     .configurationSource(new CorsConfiguration())
                 )
 
+                //csrf configuration
                 .csrf(csrf -> csrf
                     .disable()
                 )
 
+                //end points configuration and roles
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/residue").permitAll()
+                        .requestMatchers("/residue","/h2/**").permitAll()
                     .anyRequest().authenticated()
+
                 )
 
+                //configuration to h2 database
+                .headers(header-> header
+                                .frameOptions(frame -> frame.disable())
+                )
+
+                //default login page configuration
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
@@ -42,4 +69,6 @@ public class SecurityConfiguration {
 
         return http.build();
     }
+
+
 }
---
+++ src/main/java/com/victorvilar/projetoempresa/domain/SystemUser.java
@@ -0,0 +1,9 @@
+package com.victorvilar.projetoempresa.domain;
+
+import jakarta.persistence.Entity;
+import org.springframework.context.annotation.Configuration;
+
+@Entity
+public class SystemUser {
+
+}
