8842cd442f4ba6304524793f60d5dad672824a1a
Victor Vilar <victor.svilar@gmail.com>
2023-07-19 18:14:34 -0300

    fix:alterado teste do CpfCnpjValidator.

--- src/test/java/com/victorvilar/projetoempresa/util/ClassCpfCnpjValidatorTest.java
+++ src/test/java/com/victorvilar/projetoempresa/util/ClassCpfCnpjValidatorTest.java
@@ -2,12 +2,13 @@ package com.victorvilar.projetoempresa.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 //@SpringBootTest
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class ClassCpfCnpjValidatorTest {
 
 
