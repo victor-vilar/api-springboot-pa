9288fbd1709ccb9ce3c731a6c62d4de2354d8ceb
Victor Vilar <victor.svilar@gmail.com>
2023-07-20 16:16:50 -0300

    fix:removido id do EquipmentCreateDto e incluido no EquipmentUpdateDto.

--- src/main/java/com/victorvilar/projetoempresa/controllers/dto/equipment/EquipmentUpdateDto.java
+++ src/main/java/com/victorvilar/projetoempresa/controllers/dto/equipment/EquipmentUpdateDto.java
@@ -3,22 +3,31 @@ package com.victorvilar.projetoempresa.controllers.dto.equipment;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public class EquipmentCreateDto {
+public class EquipmentUpdateDto {
 
-
+    @NotBlank(message="An equipment to update must have an id")
+    private Long id;
     @NotBlank(message="The equipment must have a name")
     private String equipmentName;
     @NotNull(message="The equipment must have a size")
     private double sizeInMeterCubic;
 
-    public EquipmentCreateDto() {
+    public EquipmentUpdateDto() {
     }
 
-    public EquipmentCreateDto( String equipmentName, double sizeInMeterCubic) {
+    public EquipmentUpdateDto(Long id, String equipmentName, double sizeInMeterCubic) {
+        this.id = id;
         this.equipmentName = equipmentName;
         this.sizeInMeterCubic = sizeInMeterCubic;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getEquipmentName() {
         return equipmentName;
