1c40961a30dc5f826a877c9689045b4007f86a33
Victor Vilar <victor.svilar@gmail.com>
2023-07-20 15:53:56 -0300

    crido AddressUpdateDto e removido id do AddressCreateDto.

--- src/main/java/com/victorvilar/projetoempresa/controllers/dto/adress/AddressUpdateDto.java
+++ src/main/java/com/victorvilar/projetoempresa/controllers/dto/adress/AddressUpdateDto.java
@@ -1,8 +1,8 @@
 package com.victorvilar.projetoempresa.controllers.dto.adress;
 
-public class AddressCreateDto {
+public class AddressUpdateDto {
 
-
+    private Long id;
     private String addressName;
     private String addressNumber;
     private String complement;
@@ -12,21 +12,11 @@ public class AddressCreateDto {
     private boolean requiresCollection;
     private String customerId;
 
-
-    public AddressCreateDto() {
-
+    public AddressUpdateDto() {
     }
 
-    public AddressCreateDto(
-                            String addressName,
-                            String addressNumber,
-                            String complement,
-                            String zipCode,
-                            String city,
-                            String state,
-                            boolean requiresCollection,
-                            String customerId) {
-
+    public AddressUpdateDto(Long id, String addressName, String addressNumber, String complement, String zipCode, String city, String state, boolean requiresCollection, String customerId) {
+        this.id = id;
         this.addressName = addressName;
         this.addressNumber = addressNumber;
         this.complement = complement;
@@ -37,76 +27,75 @@ public class AddressCreateDto {
         this.customerId = customerId;
     }
 
+    public Long getId() {
+        return id;
+    }
 
-    //getters e setters - addressName
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getAddressName() {
         return addressName;
     }
+
     public void setAddressName(String addressName) {
         this.addressName = addressName;
     }
-    //--------------
 
-    //getters e setters - addressNumber
     public String getAddressNumber() {
         return addressNumber;
     }
+
     public void setAddressNumber(String addressNumber) {
         this.addressNumber = addressNumber;
     }
-    //--------------
 
-    //getters e setters - complement
     public String getComplement() {
         return complement;
     }
+
     public void setComplement(String complement) {
         this.complement = complement;
     }
-    //--------------
 
-    //getters e setters - requiredCollection
     public String getZipCode() {
         return zipCode;
     }
+
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
-    //--------------
 
-    //getters e setters - city
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
-    //--------------
 
-    //getters e setters - state
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
-    //--------------
 
-    //getters e setters - requiredCollection
     public boolean isRequiresCollection() {
         return requiresCollection;
     }
+
     public void setRequiresCollection(boolean requiresCollection) {
         this.requiresCollection = requiresCollection;
     }
-    //--------------
 
-    //getters and setters - customerId
     public String getCustomerId() {
         return customerId;
     }
+
     public void setCustomerId(String customerId) {
         this.customerId = customerId;
     }
-    //--------------
 }
