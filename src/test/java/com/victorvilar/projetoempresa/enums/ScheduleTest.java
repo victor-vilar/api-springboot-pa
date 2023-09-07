2f93a0ae7ebc4ecb54072c774a09dec49c3127bf
Victor Vilar <victor.svilar@gmail.com>
2023-09-07 16:05:21 -0300

    feat:criado teste que busca o id passando a constante.

--- src/test/java/com/victorvilar/projetoempresa/enums/ScheduleTest.java
+++ src/test/java/com/victorvilar/projetoempresa/enums/ScheduleTest.java
@@ -11,8 +11,8 @@ class ScheduleTest {
     @DisplayName("find the schedule by schedule id")
     void findScheduleSucessfully_WhenPassingValidDayId(){
 
-        Schedule diario = Schedule.getScheduleById(1);
-        Schedule semanal = Schedule.getScheduleById(2);
+        Schedule diario = Schedule.getById(1);
+        Schedule semanal = Schedule.getById(2);
 
         assertEquals(diario, Schedule.DIARIO);
         assertEquals(semanal, Schedule.SEMANAL);
@@ -24,11 +24,30 @@ class ScheduleTest {
     @DisplayName("find the schedule by schedule name")
     void  findScheduleSucessfully_WhenPassingValidDayName() {
 
-        Schedule diario = Schedule.getScheduleByName("diário");
-        Schedule semanal = Schedule.getScheduleByName("semanal");
+        Schedule diario = Schedule.getByName("diário");
+        Schedule semanal = Schedule.getByName("semanal");
 
         assertEquals(diario, Schedule.DIARIO);
         assertEquals(semanal, Schedule.SEMANAL);
+
+    }
+
+    @Test
+    @DisplayName("find the schedule id by schedule constant")
+    void  findScheduleIdSucessfully_WhenPassingValidScheduleConstant() {
+
+        int diario = Schedule.getEnumId(Schedule.DIARIO);
+        int semanal = Schedule.getEnumId(Schedule.SEMANAL);
+        int quinzenal = Schedule.getEnumId(Schedule.QUINZENAL);
+        int mensal = Schedule.getEnumId(Schedule.MENSAL);
+        int sobSolicitacao = Schedule.getEnumId(Schedule.SOB_SOLICITACAO);
+
+        assertEquals(diario,1);
+        assertEquals(semanal,2);
+        assertEquals(quinzenal,3);
+        assertEquals(mensal,4);
+        assertEquals(sobSolicitacao,5);
+
 
     }
 }
