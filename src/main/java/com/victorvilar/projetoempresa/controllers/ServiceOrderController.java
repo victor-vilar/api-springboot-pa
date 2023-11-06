2d70c1ecb0cb34d5de7e91622ff89a6b21d28ff5
Victor Vilar <victor.svilar@gmail.com>
2023-11-06 14:25:17 -0300

    feat:criado serviço e controller.

---
+++ src/main/java/com/victorvilar/projetoempresa/controllers/ServiceOrderController.java
@@ -0,0 +1,21 @@
+package com.victorvilar.projetoempresa.controllers;
+
+import com.victorvilar.projetoempresa.dto.adress.AddressCreateDto;
+import com.victorvilar.projetoempresa.dto.adress.AddressResponseDto;
+import com.victorvilar.projetoempresa.dto.adress.AddressUpdateDto;
+import com.victorvilar.projetoempresa.services.ServiceOrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/service-order")
+public class ServiceOrderController {
+
+    private final ServiceOrderService serviceOrderService;
+
+}
---
+++ src/main/java/com/victorvilar/projetoempresa/services/ServiceOrderService.java
@@ -0,0 +1,7 @@
+package com.victorvilar.projetoempresa.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceOrderService {
+}
