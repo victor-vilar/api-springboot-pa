54b48424f1489043c7da4564a5f26e7a34f1bbab
Victor Vilar <victor.svilar@gmail.com>
2023-11-07 16:03:12 -0300

    feat:criado metodo para buscar os por cliente, criado sql para inserir alguns veiculos para teste.

--- src/main/java/com/victorvilar/projetoempresa/repository/ServiceOrderRepository.java
+++ src/main/java/com/victorvilar/projetoempresa/repository/ServiceOrderRepository.java
@@ -3,5 +3,10 @@ package com.victorvilar.projetoempresa.repository;
 import com.victorvilar.projetoempresa.domain.ServiceOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
+
+    public List<ServiceOrder> findByCustomerCpfCnpj(String customerId);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,14 +2,21 @@ INSERT INTO residues(type,description)VALUES('EXTRAORDINÁRIO','CLASSE IIA');
 INSERT INTO residues(type,description)VALUES('ENTULHO','CLASSE IIB');
 INSERT INTO equipments(equipment,size_cubic)VALUES('CONTAINER 1.2M³','1.2');
 INSERT INTO equipments(equipment,size_cubic)VALUES('CONTAINER 240L','0.24');
+
+INSERT INTO vehicles(type,plate,renavam,valid_crlv_url)VALUES('CAMINHAO_COMPACTADOR','XXX-0000','00000','/xxx0000-2023.jpg');
+INSERT INTO vehicles(type,plate,renavam,valid_crlv_url)VALUES('CAMINHAO_POLIGUINDASTE','XXX-1111','1111','/xxx1111-2023.jpg');
+
 INSERT INTO application_users(username,password,profile_photo_url)VALUES('madruga','mama','/assets/madruga-profile.jpg');
 INSERT INTO application_users(username,password,profile_photo_url)VALUES('chaves','cha','/assets/chaves-profile.jpg');
+
 INSERT INTO roles(role_name)VALUES('ADMIN');
 INSERT INTO roles(role_name)VALUES('CUSTOMER');
 INSERT INTO roles(role_name)VALUES('MANAGER');
+
 INSERT INTO users_roles(application_user_id,role_id)VALUES(1,1);
 INSERT INTO users_roles(application_user_id,role_id)VALUES(1,3);
 INSERT INTO users_roles(application_user_id,role_id)VALUES(2,2);
+
 INSERT INTO clients(cpf_cnpj,name_company_name,is_active)VALUES('08454836000178','EMPRESA 1',true);
 INSERT INTO clients(cpf_cnpj,name_company_name,is_active)VALUES('29439167000135','INSTITUTO MILITAR DE ESCOTEIROS DE APARTAMENTO',true);
 INSERT INTO clients(cpf_cnpj,name_company_name,is_active)VALUES('43715989000122','COMANDO DOS PODERES FRACOS DE OPERAÇÕES ESPECIALIZADAS EM ABSTRAÇÕES BASTANTE ESPECIFICAS',true);
